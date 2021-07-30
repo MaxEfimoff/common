a801ffb12316b5e5cb2b3743cda816f5df1574f1
MaxEfimoff <34839808+MaxEfimoff@users.noreply.github.com>
2021-07-30 09:25:54 +0400

    Updates

--- src/events/team/team-updated-event.ts
+++ src/events/team/team-updated-event.ts
@@ -1,14 +1,14 @@
 import { Subjects } from '../subjects';
 export interface TeamUpdatedEvent {
-    subject: Subjects.TeamUpdated;
-    data: {
-        id: string;
-        title: string;
-        description?: string;
-        avatar?: string;
-        profile_id: string;
-        members?: object[];
-        groups?: object[];
-        version: number;
-    };
+  subject: Subjects.TeamUpdated;
+  data: {
+    id: string;
+    title: string;
+    description?: string;
+    avatar?: string;
+    profile_id: string;
+    members?: any[];
+    groups?: any[];
+    version: number;
+  };
 }
