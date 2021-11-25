7479ca59440377afa207725824380f83d011e668
MaxEfimoff <34839808+MaxEfimoff@users.noreply.github.com>
2021-11-25 15:35:39 +0400

    Updates

--- src/events/profile/profile-updated-event.ts
+++ src/events/profile/profile-updated-event.ts
@@ -1,20 +1,20 @@
 import { Subjects } from '../subjects';
 export interface ProfileUpdatedEvent {
-    subject: Subjects.ProfileUpdated;
-    data: {
-        id: string;
-        user_id: string;
-        subscribers?: [];
-        subscribedProfiles?: any[];
-        subscribedBlogs?: [];
-        joinedGroups?: [];
-        joinedTeams?: any[];
-        handle: string;
-        avatar?: string;
-        background?: string;
-        status?: string;
-        karma?: number;
-        date?: number;
-        version: number;
-    };
+  subject: Subjects.ProfileUpdated;
+  data: {
+    id: string;
+    user_id?: string;
+    subscribers?: [];
+    subscribedProfiles?: any[];
+    subscribedBlogs?: [];
+    joinedGroups?: [];
+    joinedTeams?: any[];
+    handle?: string;
+    avatar?: string;
+    background?: string;
+    status?: string;
+    karma?: number;
+    date?: number;
+    version: number;
+  };
 }
