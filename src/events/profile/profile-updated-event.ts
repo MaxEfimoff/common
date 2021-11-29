5a5f64a0d0b9ee5cbc2285856ef0d0ed609d25ec
MaxEfimoff <34839808+MaxEfimoff@users.noreply.github.com>
2021-11-29 13:10:24 +0400

    Updates

--- src/events/profile/profile-updated-event.ts
+++ src/events/profile/profile-updated-event.ts
@@ -8,7 +8,9 @@ export interface ProfileUpdatedEvent {
     subscribedProfiles?: any[];
     subscribedBlogs?: any[];
     joinedGroups?: any[];
+    myGroups?: any[];
     joinedTeams?: any[];
+    myTeams?: any[];
     handle?: string;
     avatar?: string;
     background?: string;
